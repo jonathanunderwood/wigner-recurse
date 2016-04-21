e306169a7cf4fa8281d51f81faa5f2b844a2412e
Jonathan Underwood <jonathan.underwood@gmail.com>
2016-04-21 14:36:56 +0100

    Add recurse.h

---
+++ recurse.h
@@ -0,0 +1,9 @@
+#ifndef __RECURE_H__
+#define __RECURE_H__
+
+void wigner3j_family_j (const int two_j2, const int two_j3, 
+			const int two_m2, const int two_m3,
+			double **family, int *two_jmin, int *two_jmax);
+
+#endif /* __RECURE_H__ */
+
