9ed0ebed1bf7cb511183535a2c62a59866c5a74c
Jonathan Underwood <jonathan.underwood@gmail.com>
2016-04-25 10:19:58 +0100

    Run indent over test.c

--- test.c
+++ test.c
@@ -37,10 +37,10 @@ check_3j_family_j_exact_1 (const int two_jmax)
 {
   int two_j;
 
-  for (two_j = 0; two_j <=two_jmax; two_j = (two_j + 1)*100-1)
+  for (two_j = 0; two_j <= two_jmax; two_j = (two_j + 1) * 100 - 1)
     {
       double exact = 1.0 / sqrt (two_j + 1.0);
-      double gsl = gsl_sf_coupling_3j(two_j, two_j, 0, two_j, -two_j, 0);
+      double gsl = gsl_sf_coupling_3j (two_j, two_j, 0, two_j, -two_j, 0);
       double *a;
       int tjmax, tjmin;
       size_t dim;
@@ -48,20 +48,21 @@ check_3j_family_j_exact_1 (const int two_jmax)
       dim = wigner3j_family_j_dim (two_j, two_j, two_j, -two_j);
       a = malloc (dim * sizeof (double));
       if (a == NULL)
-        {
-          fprintf (stderr, "%s:%d memory allocation error\n", __FILE__, __LINE__);
-          return __FAILURE;
-        }
+	{
+	  fprintf (stderr, "%s:%d memory allocation error\n", __FILE__,
+		   __LINE__);
+	  return __FAILURE;
+	}
 
       wigner3j_family_j (two_j, two_j, two_j, -two_j, &a, &tjmax, &tjmin);
 
       printf ("two_j: %d\trecursive: % .18e\t exact: % .18e\tdiff: % .18e\n",
-	      two_j, a[0], exact, a[0]-exact);
+	      two_j, a[0], exact, a[0] - exact);
 
       printf ("two_j: %d\t gsl: % .18e\t exact: % .18e\tdiff: % .18e\n",
-	      two_j, gsl, exact, gsl-exact);
+	      two_j, gsl, exact, gsl - exact);
 
-      
+
       free (a);
     }
 
@@ -73,12 +74,12 @@ check_3j_family_j_exact_2 (const int two_jmax)
 {
   int two_j;
 
-  printf("two_j\trecursive\tgsl\texact\trec-ex\tgsl-ex\n");
+  printf ("two_j\trecursive\tgsl\texact\trec-ex\tgsl-ex\n");
 
-  for (two_j = 0; two_j <=two_jmax; two_j = (two_j + 1)*100)
+  for (two_j = 0; two_j <= two_jmax; two_j = (two_j + 1) * 100)
     {
       double exact = 1.0 / sqrt (two_j + 1.0);
-      double gsl = gsl_sf_coupling_3j(two_j, two_j, 0, 0, 0, 0);
+      double gsl = gsl_sf_coupling_3j (two_j, two_j, 0, 0, 0, 0);
       double *a;
       int tjmax, tjmin;
       size_t dim;
@@ -86,15 +87,17 @@ check_3j_family_j_exact_2 (const int two_jmax)
       dim = wigner3j_family_j_dim (two_j, two_j, 0, 0);
       a = malloc (dim * sizeof (double));
       if (a == NULL)
-        {
-          fprintf (stderr, "%s:%d memory allocation error\n", __FILE__, __LINE__);
-          return __FAILURE;
-        }
+	{
+	  fprintf (stderr, "%s:%d memory allocation error\n", __FILE__,
+		   __LINE__);
+	  return __FAILURE;
+	}
 
       wigner3j_family_j (two_j, two_j, 0, 0, &a, &tjmax, &tjmin);
 
-      printf("%d\t%g\t%g\t%g\t%g\t%g\n", two_j, a[0],gsl, exact, a[0]-exact, gsl-exact);
-      
+      printf ("%d\t%g\t%g\t%g\t%g\t%g\n", two_j, a[0], gsl, exact,
+	      a[0] - exact, gsl - exact);
+
       free (a);
     }
 
@@ -106,23 +109,25 @@ hammer_3j_j (const int two_jmax)
 {
   double *a;
   int two_j2, two_j3, two_m2, two_m3, tjmax, tjmin;
-  
-  for (two_j2 = 0; two_j2 <= two_jmax; two_j2++)
-    for (two_m2 = -two_j2; two_m2 <=two_j2; two_m2 += 2)
-      for (two_j3 = 0; two_j3 <= two_jmax; two_j3++)
-	for (two_m3 = -two_j3; two_m3 <=two_j3; two_m3+=2)
-	  {
-            size_t dim;
 
-            dim = wigner3j_family_j_dim (two_j2, two_j3, two_m2, two_m3);
-            a = malloc (dim * sizeof (double));
-            if (a == NULL)
-              {
-                fprintf (stderr, "%s:%d memory allocation error\n", __FILE__, __LINE__);
-                return __FAILURE;
-              }
+  for (two_j2 = 0; two_j2 <= two_jmax; two_j2++)
+    for (two_m2 = -two_j2; two_m2 <= two_j2; two_m2 += 2)
+      for (two_j3 = 0; two_j3 <= two_jmax; two_j3++)
+	for (two_m3 = -two_j3; two_m3 <= two_j3; two_m3 += 2)
+	  {
+	    size_t dim;
+
+	    dim = wigner3j_family_j_dim (two_j2, two_j3, two_m2, two_m3);
+	    a = malloc (dim * sizeof (double));
+	    if (a == NULL)
+	      {
+		fprintf (stderr, "%s:%d memory allocation error\n", __FILE__,
+			 __LINE__);
+		return __FAILURE;
+	      }
 	    printf ("%d %d %d %d\n", two_j2, two_j3, two_m2, two_m3);
-	    wigner3j_family_j (two_j2, two_j3, two_m2, two_m3, &a, &tjmax, &tjmin);
+	    wigner3j_family_j (two_j2, two_j3, two_m2, two_m3, &a, &tjmax,
+			       &tjmin);
 	    free (a);
 	  }
 
@@ -130,7 +135,7 @@ hammer_3j_j (const int two_jmax)
 }
 
 int
-check_3j_family_j (const int two_j1, const int two_j2, 
+check_3j_family_j (const int two_j1, const int two_j2,
 		   const int two_m1, const int two_m2)
 {
   double *a;
@@ -145,16 +150,17 @@ check_3j_family_j (const int two_j1, const int two_j2,
       return __FAILURE;
     }
 
-  wigner3j_family_j (two_j1, two_j2, two_m1, two_m2, &a, &two_jmin, &two_jmax);
+  wigner3j_family_j (two_j1, two_j2, two_m1, two_m2, &a, &two_jmin,
+		     &two_jmax);
 
-  imax = (two_jmax-two_jmin)/2;
+  imax = (two_jmax - two_jmin) / 2;
 
-  printf ("two_j1: %d two_j2: %d two_m1: %d two_m2: %d\n", 
+  printf ("two_j1: %d two_j2: %d two_m1: %d two_m2: %d\n",
 	  two_j1, two_j2, two_m1, two_m2);
 
-  for(i=0; i<=imax; i++)
+  for (i = 0; i <= imax; i++)
     {
-      int j=two_jmin+i;
+      int j = two_jmin + i;
       printf ("\ttwo_j3: %d\t %g\n", j, a[i]);
     }
 
@@ -164,7 +170,7 @@ check_3j_family_j (const int two_j1, const int two_j2,
 }
 
 int
-check_3j_family_j_gsl (const int two_j1, const int two_j2, 
+check_3j_family_j_gsl (const int two_j1, const int two_j2,
 		       const int two_m1, const int two_m2)
 {
   double *a;
@@ -182,28 +188,28 @@ check_3j_family_j_gsl (const int two_j1, const int two_j2,
     }
 
   ret = wigner3j_family_j (two_j1, two_j2, two_m1, two_m2,
-                           &a, &two_jmin, &two_jmax);
+			   &a, &two_jmin, &two_jmax);
 
   if (ret)
     {
-      printf("Invalid angular momenta\n");
-      free(a);
+      printf ("Invalid angular momenta\n");
+      free (a);
       return __FAILURE;
     }
 
-  imax = (two_jmax-two_jmin)/2;
+  imax = (two_jmax - two_jmin) / 2;
 
-  printf ("two_j1: %d two_j2: %d two_m1: %d two_m2: %d\n", 
+  printf ("two_j1: %d two_j2: %d two_m1: %d two_m2: %d\n",
 	  two_j1, two_j2, two_m1, two_m2);
 
-  for(i=0; i<=imax; i++)
+  for (i = 0; i <= imax; i++)
     {
-      int two_j3=two_jmin+i*2;
-      
-      double gsl=gsl_sf_coupling_3j(two_j1, two_j2, two_j3,
-				    two_m1, two_m2, two_m3);
-      printf ("\ttwo_j3: %d\t\twigner: %g\tgsl: %g\tdiff: %g\n", 
-	      two_j3, a[i], gsl, a[i]-gsl);
+      int two_j3 = two_jmin + i * 2;
+
+      double gsl = gsl_sf_coupling_3j (two_j1, two_j2, two_j3,
+				       two_m1, two_m2, two_m3);
+      printf ("\ttwo_j3: %d\t\twigner: %g\tgsl: %g\tdiff: %g\n",
+	      two_j3, a[i], gsl, a[i] - gsl);
     }
 
   free (a);
@@ -213,7 +219,7 @@ check_3j_family_j_gsl (const int two_j1, const int two_j2,
 
 int
 check_3j_family_m_gsl (const int two_j1, const int two_j2,
-                       const int two_j3, const int two_m1)
+		       const int two_j3, const int two_m1)
 {
   double *a;
   int two_mmin, two_mmax, imax, i;
@@ -227,20 +233,21 @@ check_3j_family_m_gsl (const int two_j1, const int two_j2,
       return __FAILURE;
     }
 
-  wigner3j_family_m (two_j1, two_j2, two_j3, two_m1, &a, &two_mmin, &two_mmax);
+  wigner3j_family_m (two_j1, two_j2, two_j3, two_m1, &a, &two_mmin,
+		     &two_mmax);
 
-  imax = (two_mmax-two_mmin)/2;
+  imax = (two_mmax - two_mmin) / 2;
 
   printf ("two_j1: %d two_j2: %d two_j3: %d two_m1: %d\n",
 	  two_j1, two_j2, two_j3, two_m1);
 
-  for(i=0; i<=imax; i++)
+  for (i = 0; i <= imax; i++)
     {
-      int two_m=two_mmin+i*2;
-      double gsl=gsl_sf_coupling_3j(two_j1, two_j2, two_j3,
-				    two_m1, two_m, -two_m1 - two_m);
+      int two_m = two_mmin + i * 2;
+      double gsl = gsl_sf_coupling_3j (two_j1, two_j2, two_j3,
+				       two_m1, two_m, -two_m1 - two_m);
       printf ("\ttwo_m: %d\t\twigner: %g\tgsl: %g\tdiff: %g\n",
-	      two_m, a[i], gsl, a[i]-gsl);
+	      two_m, a[i], gsl, a[i] - gsl);
     }
 
   free (a);
@@ -262,14 +269,14 @@ main ()
   check_3j_family_j_gsl (9, 8, -1, 0);
   check_3j_family_j_gsl (90, 80, -30, 10);
 
-  printf("These should all be NAN, since j is half integer, m is 0\n");
+  printf ("These should all be NAN, since j is half integer, m is 0\n");
   check_3j_family_j_gsl (99, 99, 0, 0);
 
   check_3j_family_j_gsl (99, 99, 1, 1);
 
   check_3j_family_j_gsl (100, 100, 0, 0);
 
-  check_3j_family_j_exact_1(1e4);
+  check_3j_family_j_exact_1 (1e4);
   //check_3j_family_j_exact_2(10000);
 
 /*   free (a); */
@@ -284,4 +291,3 @@ main ()
 
   return 0;
 }
-
